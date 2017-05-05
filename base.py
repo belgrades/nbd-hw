a24bfefd4e9503548c10ae7413984cf102537e91
belgrades <fcremaldc@gmail.com>
2017-05-06 00:40:34 +0200

    Adding comments to base.py

--- base.py
+++ base.py
@@ -3,12 +3,20 @@ from itertools import combinations
 from random import random, randrange, sample
 import matplotlib.pyplot as plt
 
+'''
+Requeriments:
+    pip install networkx
+    pip install matplotlib
+'''
 
 def draw_our_graph(G):
     nx.draw(G, with_labels = True)
 
 def r_regular(n, r):
     def get_index(idx, r, n):
+        # IF we are at node 0 and r = 4,  we need to add nodes 1 and 2. (Remember r/2)
+        # But if we are at node n - 1, we need to add nodes n and 0. Therefore,
+        # if idx + r exceeds n - 1 we add idx + r - n
         return idx + r if idx + r < n else idx + r - n
     
     # Input
@@ -25,10 +33,12 @@ def r_regular(n, r):
 
     # Generate r-regular graph
     # We need to create nr edges
-
+    # First we need a random order of the nodes
     random_order = sample(G.nodes(), n)
 
     for idx, x in enumerate(random_order):
+        # We add r/2 edges on the "right" of every node
+        # As we have n edges, we'll have n*r edges in the end
         for idy in range(1, int(r/2)+1):
             idz = get_index(idx, idy, n)
             G.add_edge(x, random_order[idz])
@@ -52,6 +62,7 @@ def p_ER(n, p):
 
     return G
 
+# Parameters for p_ER
 n, p = 10, 0.25
 
 myGraph = p_ER(n, p)
@@ -61,6 +72,8 @@ print(len(myGraph.edges())/(n*(n-1)/2.0))
 draw_our_graph(myGraph)
 plt.show()   
 
+
+# Parameters for r_regular
 n, r = 8, 4 
 
 myGraph = r_regular(n, r)
